89bdc3c662d484b71b9456b315134752c2b6e279
Alex Cyrillo <99913928+alexcyrillo@users.noreply.github.com>
2023-09-29 17:54:49 -0300

    Create A.java

---
+++ A.java
@@ -0,0 +1,23 @@
+public class A {
+  private int A1;
+  private float A2;
+
+  public int getA1(){
+    return A1;
+  }
+  public int getA2(){
+    return A2;
+  }
+  public int setA1(int A1){
+    this.A1 = A1;
+  }
+  public int setA2(int A2){
+    this.A2 = A2;
+  }
+  public void MA1(){
+    System.out.print("MA1);
+  }
+  public void MA2(){
+    System.out.print("MA2);
+  }
+}

a8ef0009cb2c9e7b85e7a460357986e5962616d4
Alex Cyrillo <99913928+alexcyrillo@users.noreply.github.com>
2023-09-29 18:28:17 -0300

    Update A.java

--- A.java
+++ A.java
@@ -2,22 +2,26 @@ public class A {
   private int A1;
   private float A2;
 
-  public int getA1(){
+  public int getA1() {
     return A1;
   }
-  public int getA2(){
+
+  public float getA2() {
     return A2;
   }
-  public int setA1(int A1){
+
+  public void setA1(int A1) {
     this.A1 = A1;
   }
-  public int setA2(int A2){
+
+  public void setA2(int A2) {
     this.A2 = A2;
   }
-  public void MA1(){
+
+  public void MA1() {
     System.out.print("MA1");
   }
-  public void MA2(){
+
+  public void MA2() {
     System.out.print("MA2");
   }
-}

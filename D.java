cb4b5509cec8e4ba372e8eb51d6460e1f2137c82
Alex Cyrillo <alex.csborges@hotmail.com>
2023-09-30 17:00:49 -0300

    Criacao Class D

---
+++ D.java
@@ -0,0 +1,12 @@
+public class D {
+   private int D1;
+   private int D2;
+
+   public void MD1() {
+      System.out.print("MD1");
+   }
+
+   public void MD2() {
+      System.out.print("MD2");
+   }
+}
